2dd911d4f4a398fb46e34eff2b11c8d6d1dd87b7
domi <m.doerflinger@hs-furtwangen.de>
2021-11-17 13:33:43 +0100

    first commit

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Nicht schlecht, oder?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Nicht schlecht, oder?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Oh, das ist aber schade. Naja, dann bis zum nächsten Mal! Tschüss!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Oh, das ist aber schade. Naja, dann bis zum nächsten Mal! Tschüss!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Naja, ich bin ja auch ein I T-Produkt! Sogar ein ziemlich komplexes. 
+Hier schauen Sie mal: ich kann sogar Luft-Saxophon spielen.
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Naja, ich bin ja auch ein I T-Produkt! Sogar ein ziemlich komplexes. 
+Hier schauen Sie mal: ich kann sogar Luft-Saxophon spielen.
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ah, sehr interessant. ITP ist eine ganz wichtige Disziplin der angewandten Informatik.
+Da geht es um IT-Produkte und Projekte. Alles sehr spannend! Ich bin übrigens Pepper,
+ein humano ider Roboter, und ich würde mich
+auch über einen smarten Produkt-Manager für mich freuen. 
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ah, sehr interessant. ITP ist eine ganz wichtige Disziplin der angewandten Informatik.
+Da geht es um IT-Produkte und Projekte. Alles sehr spannend! Ich bin übrigens Pepper,
+ein humano ider Roboter, und ich würde mich
+auch über einen smarten Produkt-Manager für mich freuen. 
+</translation>
+        </message>
+    </context>
+</TS>
